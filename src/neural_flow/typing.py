35351100bc486bedd03b3275b039c4f7adc5a68f
Hans Dembinski <hans.dembinski@gmail.com>
2024-02-15 18:38:05 +0100

    wip

--- src/neural_flow/typing.py
+++ src/neural_flow/typing.py
@@ -1,7 +1,8 @@
 """Types for NeuralFlow."""
 
-from typing import Union, Tuple, List
+from typing import Union, Tuple, List, Any
 
 # define a type alias for Jax Pytrees
 Pytree = Union[Tuple, List]
 Bijector_Info = Tuple[str, tuple]
+Array = Any
