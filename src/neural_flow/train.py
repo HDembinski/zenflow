48f297f9312e216df81ef9507bdd51652000be28
Hans Dembinski <hans.dembinski@gmail.com>
2024-02-15 17:26:29 +0100

    works

--- src/neural_flow/train.py
+++ src/neural_flow/train.py
@@ -27,9 +27,7 @@ def train(
     """Trains the normalizing flow on the provided inputs."""
     # split the seed
     root_key = random.PRNGKey(seed)
-    init_key, batch_key, bijector_key = random.split(root_key, num=3)
-
-    # if no loss_fn is provided, use the default loss function
+    init_key, batch_key = random.split(root_key)
 
     @jit
     def loss_fn(params, x, c):
@@ -66,17 +64,17 @@ def train(
 
     for epoch in loop:
         # new permutation of batches
-        permute_key, sample_key, batch_key = random.split(batch_key, num=3)
+        permute_key, batch_key = random.split(batch_key)
 
         perm = random.permutation(permute_key, X_train.shape[0])
-        X = X_train[perm]
-        C = C_train[perm]
+        X_perm = X_train[perm]
+        C_perm = C_train[perm]
 
         # loop through batches and step optimizer
-        for batch_idx in range(0, len(X), batch_size):
-            Xb = X[batch_idx : batch_idx + batch_size]
-            Cb = C[batch_idx : batch_idx + batch_size]
-            params, opt_state = step(params, opt_state, Xb, Cb)
+        for batch_idx in range(0, len(X_perm), batch_size):
+            X = X_perm[batch_idx : batch_idx + batch_size]
+            C = C_perm[batch_idx : batch_idx + batch_size]
+            params, opt_state = step(params, opt_state, X, C)
 
         losses.append(loss_fn(params, X, C).item())
         test_losses.append(loss_fn(params, X_test, C_test).item())
@@ -87,7 +85,8 @@ def train(
 
         stop = np.isnan(losses[-1]) or (
             len(test_losses) > 2 * patience
-            and not np.min(test_losses[-patience:]) < np.min(test_losses[:-patience])
+            and not np.min(test_losses[-patience:])
+            < np.min(test_losses[-2 * patience : -patience])
         )
 
         if stop:
