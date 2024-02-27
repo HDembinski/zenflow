4843d7bd38734a1a91eee636c3f485faed549bb4
Hans Dembinski <hans.dembinski@gmail.com>
2024-02-27 15:06:42 +0100

    u

--- src/neural_flow/utils.py
+++ src/neural_flow/utils.py
@@ -5,8 +5,17 @@ from jaxtyping import Array
 import jax.numpy as jnp
 
 
+def _knots(dx, bound):
+    return jnp.pad(
+        -bound + jnp.cumsum(dx, axis=-1),
+        [(0, 0)] * (len(dx.shape) - 1) + [(1, 0)],
+        mode="constant",
+        constant_values=-bound,
+    )
+
+
 def rational_quadratic_spline(
-    inputs: Array, W: Array, H: Array, D: Array, B: float, inverse: bool
+    inputs: Array, dx: Array, dy: Array, slope: Array, bound: float, inverse: bool
 ) -> Tuple[Array, Optional[Array]]:
     """
     Apply rational quadratic spline to inputs and return outputs with log_det.
@@ -17,13 +26,13 @@ def rational_quadratic_spline(
     ----------
     inputs : jnp.ndarray
         The inputs to be transformed.
-    W : jnp.ndarray
+    dx : jnp.ndarray
         The widths of the spline bins.
-    H : jnp.ndarray
+    dy : jnp.ndarray
         The heights of the spline bins.
-    D : jnp.ndarray
+    slope : jnp.ndarray
         The derivatives of the inner spline knots.
-    B : float
+    bound : float
         Range of the splines.
         Outside of (-B,B), the transformation is just the identity.
     inverse : bool
@@ -49,28 +58,18 @@ def rational_quadratic_spline(
 
     """
     # knot x-positions
-    xk = jnp.pad(
-        -B + jnp.cumsum(W, axis=-1),
-        [(0, 0)] * (len(W.shape) - 1) + [(1, 0)],
-        mode="constant",
-        constant_values=-B,
-    )
+    xk = _knots(dx, bound)
     # knot y-positions
-    yk = jnp.pad(
-        -B + jnp.cumsum(H, axis=-1),
-        [(0, 0)] * (len(H.shape) - 1) + [(1, 0)],
-        mode="constant",
-        constant_values=-B,
-    )
+    yk = _knots(dy, bound)
     # knot derivatives
     dk = jnp.pad(
-        D,
-        [(0, 0)] * (len(D.shape) - 1) + [(1, 1)],
+        slope,
+        [(0, 0)] * (len(slope.shape) - 1) + [(1, 1)],
         mode="constant",
         constant_values=1,
     )
     # knot slopes
-    sk = H / W
+    sk = dy / dx
 
     # if not periodic, out-of-bounds inputs will have identity applied
     # if periodic, we map the input into the appropriate region inside
@@ -78,8 +77,8 @@ def rational_quadratic_spline(
     # This makes sure that out-of-bounds inputs don't cause problems
     # with the spline, but for the non-periodic case, we will replace
     # these with their original values at the end
-    out_of_bounds = (inputs <= -B) | (inputs >= B)
-    masked_inputs = jnp.where(out_of_bounds, jnp.abs(inputs) - B, inputs)
+    out_of_bounds = (inputs <= -bound) | (inputs >= bound)
+    masked_inputs = jnp.where(out_of_bounds, jnp.abs(inputs) - bound, inputs)
 
     # find bin for each input
     if inverse:
@@ -92,29 +91,29 @@ def rational_quadratic_spline(
     input_yk = jnp.take_along_axis(yk, idx, -1)[..., 0]
     input_dk = jnp.take_along_axis(dk, idx, -1)[..., 0]
     input_dkp1 = jnp.take_along_axis(dk, idx + 1, -1)[..., 0]
-    input_wk = jnp.take_along_axis(W, idx, -1)[..., 0]
-    input_hk = jnp.take_along_axis(H, idx, -1)[..., 0]
+    input_dx = jnp.take_along_axis(dx, idx, -1)[..., 0]
+    input_dy = jnp.take_along_axis(dy, idx, -1)[..., 0]
     input_sk = jnp.take_along_axis(sk, idx, -1)[..., 0]
 
     if inverse:
         # [1] Appendix A.3
         # quadratic formula coefficients
-        a = (input_hk) * (input_sk - input_dk) + (masked_inputs - input_yk) * (
+        a = (input_dy) * (input_sk - input_dk) + (masked_inputs - input_yk) * (
             input_dkp1 + input_dk - 2 * input_sk
         )
-        b = (input_hk) * input_dk - (masked_inputs - input_yk) * (
+        b = (input_dy) * input_dk - (masked_inputs - input_yk) * (
             input_dkp1 + input_dk - 2 * input_sk
         )
         c = -input_sk * (masked_inputs - input_yk)
 
         relx = 2 * c / (-b - jnp.sqrt(b**2 - 4 * a * c))
-        outputs = relx * input_wk + input_xk
+        outputs = relx * input_dx + input_xk
         log_det = None
     else:
         # [1] Appendix A.1
         # calculate spline
-        relx = (masked_inputs - input_xk) / input_wk
-        num = input_hk * (input_sk * relx**2 + input_dk * relx * (1 - relx))
+        relx = (masked_inputs - input_xk) / input_dx
+        num = input_dy * (input_sk * relx**2 + input_dk * relx * (1 - relx))
         den = input_sk + (input_dkp1 + input_dk - 2 * input_sk) * relx * (1 - relx)
         outputs = input_yk + num / den
 
